2969407d13fed3d9bae45ceb6dea3b73c1cf52df
Fernando TC <g561.p@hotmail.com>
2020-08-19 14:06:57 -0500

    new changes that allows to the program, process a numbers of two digits

--- study_rep_platzi/Curso_basico_py/Test_functions_1.py
+++ study_rep_platzi/Curso_basico_py/Test_functions_1.py
@@ -1,23 +1,29 @@
 # Function that receives a list of numbers, and returns its average.  
 
-def operation(num_list):
+def operation(list_transformed):
     op=0
     list_without_str=[]
-    for number in num_list:
+    for number in list_transformed:
         if number == ",":
             continue
         op= op+int(number)
         list_without_str.append(number)
         
     
-    print(op/len(list_without_str))
+    print(f'The result of the operation is {round(op/len(list_without_str),2)}')
     
            
 def run():
-    num_list=(list(input('Write a list of 5 numbers (just one digit and separated by ,): ')))
-    operation(num_list)
+    list_transformed=[]
+    num_list1=(int(input('Write a number: ')))
+    num_list2=(int(input('Write a second number: ')))
+    num_list3=(int(input('Write a third number: ')))
+
+    list_transformed=[num_list1,num_list2,num_list3]
+    operation(list_transformed)
 
 
 
 if __name__ == "__main__":
+    print('*Function that receives three numbers, and returns its average*')
     run()
