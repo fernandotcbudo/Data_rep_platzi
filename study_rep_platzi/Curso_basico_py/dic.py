2befc3fd3acdf32bf3e673feebd8b43b47fd8a5f
Fernando TC <g561.p@hotmail.com>
2020-08-23 12:02:06 -0500

    New changes stuying dics

--- study_rep_platzi/Curso_basico_py/dic.py
+++ study_rep_platzi/Curso_basico_py/dic.py
@@ -1,13 +1,40 @@
-def run():
-    my_dic={
-        'Usuario1':'1*af',
-        'Usuario2':'2456g',
-        'Usuario3':'5q-gg',
-    }
+#
 
-    for user,password in my_dic.items():
-        print(f'Para el usuario llamado {user} su contraseña es {password}')
+def operation(my_dic):
+
+    question_1=str(input('Do you want to change your password? (Y/N): '))
+
+    if question_1 == 'Y' or 'y':
+
+        display=int(input('''
+        1. User 1
+        2. User 2
+        3. User 3
+
+        ''')
+        
+    
+        if display == 1:
+            new_1=str(input('Write password for User 1: '))
+            my_dic.update(new_1)
+
+    
+
         
 
+
+def run():
+    my_dic={
+        'UserNumber1':'1*af',
+        'UserNumber2':'2456g',
+        'UserNumber3':'5q-gg',
+    }
+
+    for password in my_dic.values():
+        print(f'Your actual passwords are: {password}')
+
+    operation(my_dic)
+
+    
 if __name__ == "__main__":
     run()
