e7ade78653ba4e4932188e98daed448e2a9b9d53
Fernando TC <g561.p@hotmail.com>
2020-08-24 15:43:45 -0500

    Improvements in the code

--- study_rep_platzi/Curso_basico_py/dic.py
+++ study_rep_platzi/Curso_basico_py/dic.py
@@ -4,23 +4,36 @@ def operation(my_dic):
 
     question_1=str(input('Do you want to change your password? (Y/N): '))
 
-    if question_1 == 'Y' or 'y':
-
+    if question_1 == 'y':
         display=int(input('''
         1. User 1
         2. User 2
         3. User 3
-
-        ''')
-        
+         '''))
     
         if display == 1:
-            new_1=str(input('Write password for User 1: '))
-            my_dic.update(new_1)
+            dis_password_1=str(input('Write new password: '))
+            my_dic['UserNumber1'] = (dis_password_1)
+            print(f'New password: {my_dic["UserNumber1"][0:]}')
 
+        elif display ==2:
+            dis_password_2=str(input('Write new password: '))
+            my_dic['UserNumber2'] = (dis_password_2)
+            print(f'New password: {my_dic["UserNumber2"][0:]}')
+
+        elif display ==3:
+            dis_password_3=str(input('Write new password: '))
+            my_dic['UserNumber3'] = (dis_password_3)
+            print(f'New password: {my_dic["UserNumber3"][0:]}')
+
+        else:
+            print('Write a correct answer')     
+
+
+
+    else:
+        print('See you later') 
     
-
-        
 
 
 def run():
@@ -37,4 +50,5 @@ def run():
 
     
 if __name__ == "__main__":
+    print('This is an APP to change passwords')
     run()
