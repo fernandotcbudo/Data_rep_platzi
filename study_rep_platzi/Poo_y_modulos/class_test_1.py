4b22c68383f915b3e8b8c88aee19efabf434a3d0
Fernando TC <g561.p@hotmail.com>
2020-08-29 14:54:33 -0500

    Introductory exercise to practice POO

---
+++ study_rep_platzi/Poo_y_modulos/class_test_1.py
@@ -0,0 +1,32 @@
+#POO Test 
+
+class bicycle():
+
+    wells=2
+
+    def moving(self):
+        print('The bicycle starts to move')
+
+
+class car():
+
+    wells=4
+
+    def moving(self):
+        print('The car starst to move')
+
+
+
+my_bike=bicycle()
+
+print(f'My bike has {my_bike.wells} wells')
+
+my_car=car()
+
+print(f'My car has {my_car.wells} wells')
+
+print('-----------------------------------------')
+
+if __name__ == "__main__":
+    my_bike.moving()
+    my_car.moving()
