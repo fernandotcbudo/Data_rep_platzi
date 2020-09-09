15b38df0e6df189b6eca418aac0fd9479a81802a
Fernando TC <g561.p@hotmail.com>
2020-09-09 09:58:07 -0500

    New functionalities with arrays

--- study_rep_platzi/Poo_y_modulos/class_test_5.py
+++ study_rep_platzi/Poo_y_modulos/class_test_5.py
@@ -1,6 +1,6 @@
 #Exercise to practice getters and setters
-
-class product():
+#Exercise to practice arrays
+class Product():
 
     def __init__ (self,code,name,price):
         self.__code=code
@@ -40,12 +40,43 @@ class product():
     def __str__(self):
         return 'Code: ' + str(self.__code ) + ', Name: ' + str(self.__name ) + ', Price: ' + str(self.__price)
 
-p1=product(1,'Banana',12)
-p2=product(2,'Apple',20)
-p3=product(3,'Orange',18)
+class Order():
 
+    def __init__(self,products,amounts):
+        self.__products=products
+        self.__amounts=amounts  
+
+    def total_order(self):
+
+        total=0
+
+        for (p,a) in zip(self.__products,self.__amounts):
+            total= total + p.cal_total(a)
+
+        return total
+
+       
+    def show_order(self):
+        for (p,a) in zip(self.__products,self.__amounts):
+            print(f'Product: {p.name} Amount: {a}')
+
+
+p1=Product(1,'Banana',12)
+p2=Product(2,'Apple',20)
+p3=Product(3,'Orange',18)
+
+list_prod=[p1,p2,p3]
+new_amounts=[5,10,20]
+new_order=Order(list_prod,new_amounts)
 
 if __name__ == "__main__":
-    print(f'{p1} your total cost is: {p1.cal_total(5)}')
+    print('Wellcome to PRICE MARKET!')
+    print(f'{p1} total cost is of this product: {p1.cal_total(5)}')
+    print('Now you can order more!')
+    print(f'Total order: {new_order.total_order()} ')
+    new_order.show_order()
+
+
+
 
     
