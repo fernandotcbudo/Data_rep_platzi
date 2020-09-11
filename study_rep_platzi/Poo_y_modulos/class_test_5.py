7b8589f8169d2a077008970f46e12999754735b1
Fernando TC <g561.p@hotmail.com>
2020-09-10 20:30:39 -0500

    New changes to practice try and except

--- study_rep_platzi/Poo_y_modulos/class_test_5.py
+++ study_rep_platzi/Poo_y_modulos/class_test_5.py
@@ -1,5 +1,6 @@
 #Exercise to practice getters and setters
 #Exercise to practice arrays
+#Exercise to practice try and except 
 class Product():
 
     def __init__ (self,code,name,price):
@@ -42,10 +43,43 @@ class Product():
 
 class Order():
 
-    def __init__(self,products,amounts):
-        self.__products=products
-        self.__amounts=amounts  
+    def __init__(self):
+        self.__products=[]
+        self.__amounts=[]  
+#This function adds a product 
+    def add_pro (self,product,amount):
+        
+        if not isinstance(product,Product):
+            raise Exception('Product must be correct')
 
+        if not isinstance(amount,int):
+            raise Exception('Amount must be a number')
+
+        if amount <= 0:
+            raise Exception('Amount must be greater than zero')
+
+        if product in self.__products:
+            index= self.__products.index(product)
+            self.__amounts[index] = self.__amounts[index] + amount
+        else:
+            self.__products.append(product)
+            self.__amounts.append(amount)
+
+#This function removes a product 
+    def remove_pro (self,product):  
+        if not isinstance(product,Product):
+            raise Exception('Product must be correct')
+
+        if product in self.__products:
+            index= self.__products.index(product)
+            del self.__products[index]
+            del self.__amounts[index]
+
+        else:
+            raise Exception('The product does not exists')
+
+
+#This shows the sum of the cost of an order 
     def total_order(self):
 
         total=0
@@ -55,7 +89,7 @@ class Order():
 
         return total
 
-       
+ #This shows the total order       
     def show_order(self):
         for (p,a) in zip(self.__products,self.__amounts):
             print(f'Product: {p.name} Amount: {a}')
@@ -65,16 +99,35 @@ p1=Product(1,'Banana',12)
 p2=Product(2,'Apple',20)
 p3=Product(3,'Orange',18)
 
-list_prod=[p1,p2,p3]
-new_amounts=[5,10,20]
-new_order=Order(list_prod,new_amounts)
+new_order=Order()
+
+
 
 if __name__ == "__main__":
     print('Wellcome to PRICE MARKET!')
-    print(f'{p1} total cost is of this product: {p1.cal_total(5)}')
-    print('Now you can order more!')
-    print(f'Total order: {new_order.total_order()} ')
-    new_order.show_order()
+    try:
+        new_order.add_pro(p1,5)
+        new_order.add_pro(p2,5)
+        new_order.add_pro(p3,5)
+
+        print(f'total cost is of this order: {new_order.total_order()}')
+
+        new_order.show_order()
+
+        new_order.remove_pro(p1)
+
+        print(f'total cost is of this order: {new_order.total_order()}')
+
+        new_order.show_order()
+
+
+    except Exception as e:
+        print(e)
+
+
+
+
+
 
 
 
