9a1303ae3c34f4fede5164a233ff3ce03aeba1c5
Fernando TC <g561.p@hotmail.com>
2020-10-15 20:56:30 -0500

    Studying arrays

--- Curso_general/prueba.py
+++ Curso_general/prueba.py
@@ -3,9 +3,16 @@ a=0,1e-3
 print(type(a))
 print(a)
 
-#Complex number
+#Complex numbers
 b= 2,1 + 7,8j
 print(type(b))
 print(b) 
 
-    
+#Arrays 
+new_list=[22,True,'Hello',"0,2",[5,8]]
+select=new_list[4][1]
+change=new_list[0]=99
+select_2=new_list[0]
+select_3=new_list[-1:]
+print(f'Your first selection is {select} and second is {select_2} ok last selection is \n {select_3}')
+
