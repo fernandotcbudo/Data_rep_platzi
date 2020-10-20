445361cbb5382eb072e6db5fda8ae288f27908c9
Fernando TC <g561.p@hotmail.com>
2020-10-20 15:56:26 -0500

    Studying tuples and dictionaries

--- Curso_general/prueba.py
+++ Curso_general/prueba.py
@@ -16,3 +16,16 @@ select_2=new_list[0]
 select_3=new_list[-1:]
 print(f'Your first selection is {select} and second is {select_2} ok last selection is \n {select_3}')
 
+#Tuples
+new_tuple=(20,22,'Python')
+new_select=new_tuple[0]
+print(f'The value is {new_select}')
+
+#Dictionaries
+new_dic={
+    'Neck':'Guillotine',
+    'Arms':'Armbar',
+    'Knees':'Kneebar'
+}
+new_select_1=new_dic['Neck']
+print(f'Submission is {new_select_1}')
