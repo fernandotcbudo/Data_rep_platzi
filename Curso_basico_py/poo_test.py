22e7d7ce49f87810ae4f25b1fe91e0b323d7590d
Fernando TC <g561.p@hotmail.com>
2020-11-03 22:53:41 -0500

    Heritage POO study

--- Curso_basico_py/poo_test.py
+++ Curso_basico_py/poo_test.py
@@ -1,15 +1,17 @@
+#POO CLASES OBJECTS - HERITAGE
 class Robot:
+    """Class constructor Robot"""
     def __init__(self,electricity):
         self.electricity=electricity
         print(f'Amount: {electricity} ')
-
+    """Attribute start"""
     def start(self):
         if self.electricity > 0:
             print('Status of your conection is: On')
 
         else:
             print('Status of your conection is: Off')
-
+    """Attribute move"""
     def move(self):
 
         if self.electricity > 0:
@@ -18,9 +20,22 @@ class Robot:
         else:
             print('It doesnt move')
 
+class R2d2(Robot):
+    """Class constructor R2d2"""
+    def __init__(self,weapon,electricity):
+        """Inherited attribute"""
+        Robot.__init__(self,electricity)
+        self.weapon=weapon
+        print(f' Your weapon is {weapon} and your energy is {electricity}')
 
+    
 if __name__ == "__main__":
     my_robot= Robot(int(input('Write the amount of electricity: ')))
     my_robot.start()    
     my_robot.move()
+    r1=R2d2(input('Write the type of weapon: '),int(input('Write the amount of electricity: ')))
+    r1.start()
+    r1.move()
+
+    
 
