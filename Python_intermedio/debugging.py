9f2ee2a38f23b5bf743147d9befdcbc4f91ad782
Fernando TC <g561.p@hotmail.com>
2021-08-09 10:20:42 -0500

    Debugging & asserts

--- Python_intermedio/debugging.py
+++ Python_intermedio/debugging.py
@@ -1,12 +1,22 @@
-
 def divisors(num):
-    divisors=  [i for i in range(1,num+1) if num % i == 1]
-    return divisors
+    try:
+        if num == 0:
+            raise ValueError('Empty string')
+        divisors=  [i for i in range(1,num+1) if num % i == 0]
+        return divisors
+
+    except ValueError as ve:
+        print(ve)
+        return False 
 
 
 def run():
+    try:
      num= int(input('Write a number: '))
      print(divisors(num))
+     print('Program is over')
+    except ValueError:
+     print('Please write a number')   
 
 if __name__ == '__main__':
     run()
