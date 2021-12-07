b35fb806419ea8ffc4221d64d28cff424f475891
Fernando TC <g561.p@hotmail.com>
2021-12-07 07:56:03 -0500

    Tipado estatico pyhton

--- Curso prof de python/python_pro.py
+++ Curso prof de python/python_pro.py
@@ -1,1 +1,24 @@
- 
+from typing import Dict,List
+from typing import Tuple
+
+# Listas con tipado estatico 
+positives: List[int] = [1,2,3,4,5]
+# Diccionarios con tipado estatico 
+users: Dict [str,int] = {
+    'argentina':1,
+    'colombia':2,
+}
+# Listas con diccionarios embebidos tipado estatico
+countries:List[Dict[str,str]] = [
+    {
+        'name':'Argentina',
+        'people':'656886',
+    },
+    
+    {
+        'name':'Colombia',
+        'people':'786586',
+    },  
+]
+# Tuplas con tipado estatico
+numbers: Tuple[int,float,int] = [1,0.5,5]
