138c6ef700fb4c07a53a2b7ce501b01199655e53
FernandoTC <g561.p@hotmail.com>
2022-01-10 19:18:31 -0500

    Final proyect

--- Curso prof de python/final_proyect.py
+++ Curso prof de python/final_proyect.py
@@ -2,18 +2,21 @@ from datetime import datetime
 import pytz
 import random
 
-def change_list(func_x):
-    def wrapper(any_list):
-        final_set= set(any_list)
-        final_set= list(final_set)
-        wrapper(any_list)
-    return wrapper
 
-@change_list
-def say_hi(name):
-    return f'{name} helloooo'
+def set_change(any_list):
+    
+    final_set= set(any_list)
+    list_change= list(final_set)
+    selector= random.randint(0,4)
+    name_chosen= list_change[selector]
+    bogota_timezone= pytz.timezone("America/Bogota")
+    bogota_datetime=datetime.now(bogota_timezone)
+    
+    return 'Hola ' + name_chosen + ' la fecha de hoy en Bogotá es: ' +bogota_datetime.strftime("%d/%m/%Y")
 
+def run():
+    run_func= set_change(["Diego","Paula","Fernando","Emma","Fernando","Paula","Carlos","Emma"])
+    print(run_func)
 
 if __name__ == '__main__':
-    final=change_list(["Diego","Paula","Fernando","Emma","Fernando","Paula"])
-    print(final)
+    run()
